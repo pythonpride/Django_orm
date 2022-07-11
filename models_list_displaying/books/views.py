3b771e88277076a077fe36fc0948026bc87fcbf1
Oxana Lezhneva <pythonpride@ya.ru>
2022-07-12 01:41:29 +0300

    Update views.py

--- models_list_displaying/books/views.py
+++ models_list_displaying/books/views.py
@@ -3,23 +3,17 @@ from django.shortcuts import render
 from books.models import Book
 from django.core.paginator import Paginator
 
-
-
 def books_view(request):
     template = 'books/books_list.html'
     Book_objects = Book.objects.all().order_by('pub_date').distinct()
-    context = {
-        'books':Book_objects
-    }
+    context = {'books':Book_objects}
     return render(request, template, context)
-
-
 
 def paginator_books(request, param):
     template = 'books/books_list.html'
-    pages= [book['pub_date'].strftime("%Y-%m-%d") for book in Book.objects.order_by('pub_date').values('pub_date').distinct()]
     CONTENT = Book.objects.all().order_by('pub_date')
     paginator = Paginator(CONTENT, 1)
+    pages= [book['pub_date'].strftime("%Y-%m-%d") for book in Book.objects.order_by('pub_date').values('pub_date').distinct()]
     index_page = pages.index(param)
     page = paginator.get_page(index_page+1)
     previous_page = pages[index_page-1]   
@@ -33,6 +27,5 @@ def paginator_books(request, param):
         'page': page,
         'next_page':next_page,
         'previous_page':previous_page       
-
     }
     return render(request, template, context)
